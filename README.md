073870c22d66e21eef35afdbc8e363d129745c89
babaiyu <bayuputra9a@gmail.com>
2021-01-21 21:23:48 +0700

    Added readme.md, remove gh-pages

---
+++ README.md
@@ -0,0 +1,9 @@
+# Portfolio
+
+[website](http://babaiyu.vercel.app/)
+
+## Portfolio babaiyu is created using
+
+- [TypeScript](https://www.typescriptlang.org/)
+- [Webpack](https://webpack.js.org/)
+- [Tailwind CSS](https://tailwindcss.com/)
--- publish.js
+++
@@ -1,8 +0,0 @@
-const ghPages = require('gh-pages');
-
-ghPages.publish('dist', function (err) {
-  const isError = err;
-  if (isError === undefined) {
-    console.log('Success upload to github-pages');
-  } else console.log('There is Error', isError);
-});
