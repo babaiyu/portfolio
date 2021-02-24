1bb26d0242c48b16ac1d9a75252b00456b650ed6
babaiyu <bayuputra9a@gmail.com>
2021-02-24 11:47:00 +0700

    Added looping data account

---
+++ src/mock/data.ts
@@ -0,0 +1,22 @@
+export const dataAccount = [
+  {
+    id: 1,
+    url: 'https://github.com/babaiyu',
+    logo: 'logo-github',
+  },
+  {
+    id: 2,
+    url: 'https://www.linkedin.com/in/bayu-permana-putra-8ba540170/',
+    logo: 'logo-linkedin',
+  },
+  {
+    id: 3,
+    url: 'https://www.instagram.com/babaiyu/',
+    logo: 'logo-instagram',
+  },
+  {
+    id: 4,
+    url: 'https://www.facebook.com/bayu.permanaputra.94/',
+    logo: 'logo-facebook',
+  },
+];
